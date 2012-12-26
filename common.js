df6c18dd2d4bf8a87fb61bf9b9a4e5e990e69044
Ivajkin Timofej <timatnet@gmail.com>
2012-12-26 04:03:24 -0800

    Create common.js

---
+++ common.js
@@ -0,0 +1,17 @@
+
+Array.prototype.fold = function(sum, f) {
+    for(var i = 0; i < this.length; ++i) {
+        sum = f(sum, this[i]);
+    }
+    return sum;
+};
+
+var sum = function(sum, x) {return sum + x;};
+
+function generate(N, f) {
+    var _ = [];
+    for(var i = 0; i < N; ++i) {
+        _.push(f(i));
+    }
+    return _;
+}
